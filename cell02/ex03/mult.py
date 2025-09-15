f14431cc6512c764c34b6a18715de392efb4276e
65050057-arch <65050057@kmitl.ac.th>
2025-09-15 22:08:40 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+num1 = int(input("Input first Number :"))
+num2 = int(input("Input second Number :"))
+MutiNum = num1 * num2 
+if num1 * num2 < 0:
+    print("The result is negative") 
+elif num1 * num2 > 0:
+    print("The result is positive")
+else:
+  print("The result is zero")
