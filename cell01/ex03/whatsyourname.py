fd130472233eca0937c2d5e63048a6e5616691ad
65050057-arch <65050057@kmitl.ac.th>
2025-09-15 21:35:12 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+first_Name =input("Your name:")
+last_name = input("Your last name:")
+full_name = first_Name + " " + last_name
+print ("Hey, whats your first name? :"+ first_Name+" ",
+"And your last name? :",last_name," ",
+"Well, pleased to meet you",full_name)
