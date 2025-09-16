88a1b34addccd022dfbd7fc846e7921a289982e4
65050057-arch <65050057@kmitl.ac.th>
2025-09-16 17:28:54 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,7 +1,4 @@
-first_text = input("What you gotta say?")
-print(first_text)
-while True:
-    Text = input("Enter your Text: ")
-    if Text.lower() == "stop":  
-        break
-    print("I got that! Anything else?:", Text)
+text = input("What you gotta say? : ")
+while text != "STOP":
+    text = input("I got that! Anything else? : ")
+
