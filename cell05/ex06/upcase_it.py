270299c72b9f96359a2c7a1045a6cf8c248a121b
65050057-arch <65050057@kmitl.ac.th>
2025-09-16 15:31:48 +0700

    Update upcase_it.py

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -1,9 +1,7 @@
 text = input("Enter your Text:")
 words = text.split()      
 word_count = len(words)
-if word_count >1:
-    print("this exercise is quite easy!")
-elif word_count == 1:
-    print(text.upper())
+if word_count >=1:
+    print("text.upper()")
 else:
     print("none")
