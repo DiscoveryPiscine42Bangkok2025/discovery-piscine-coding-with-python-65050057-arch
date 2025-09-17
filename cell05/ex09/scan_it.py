f8b7d219ef19b655e6614ce9533b15ebefb948c4
65050057-arch <65050057@kmitl.ac.th>
2025-09-17 12:07:59 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,10 +1,15 @@
 import sys, re
+
 if len(sys.argv) != 3:
- tring = sys.argv[2]
+    print("none")
+    sys.exit()
+
+keyword = sys.argv[1]
+string = sys.argv[2]
+
 same = re.findall(re.escape(keyword), string)
+
 if same :
     print(len(same))
 else:
-    print("none")   print("none")
-    sys.exit()
-keyword = sys.argv[1]
+    print("none")
